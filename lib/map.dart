8b41a49c55fcf0999d686a0ec13c332f31136c58
Ahmed <ahmedkhelifa765@gmail.com>
2024-06-08 00:31:16 +0100

    initial commit

---
+++ lib/map.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapView extends StatefulWidget {
+  const MapView({super.key});
+
+  @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  MapBoxNavigationViewController? _controller;
+  String? _instruction;
+  bool _isMultipleStop = false;
+  double? _distanceRemaining, _durationRemaining;
+  bool _routeBuilt = false;
+  bool _isNavigating = false;
+  bool _arrived = false;
+  late MapBoxOptions _navigationOption;
+
+  Future<void> initialize() async {
+    if (!mounted) return;
+    _navigationOption = MapBoxNavigation.instance.getDefaultOptions();
+    _navigationOption.initialLatitude = 37.7749;
+    _navigationOption.initialLongitude = -122.4194;
+    _navigationOption.mode = MapBoxNavigationMode.driving;
+    MapBoxNavigation.instance.registerRouteEventListener(_onRouteEvent);
+
+    // Get the user's current position
+    try {
+      final position = await _determinePosition();
+      _navigationOption.initialLatitude = position.latitude;
+      _navigationOption.initialLongitude = position.longitude;
+    } catch (e) {
+      print('Error getting location: $e');
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location permissions are permanently denied.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  void initState() {
+    initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 1,
+            child: Container(
+              color: Colors.grey[100],
+              child: MapBoxNavigationView(
+                options: _navigationOption,
+                onRouteEvent: _onRouteEvent,
+                onCreated: (MapBoxNavigationViewController controller) async {
+                  _controller = controller;
+                  controller.initialize();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _onRouteEvent(e) async {
+    _distanceRemaining = await MapBoxNavigation.instance.getDistanceRemaining();
+    _durationRemaining = await MapBoxNavigation.instance.getDurationRemaining();
+
+    switch (e.eventType) {
+      case MapBoxEvent.progress_change:
+        var progressEvent = e.data as RouteProgressEvent;
+        _arrived = progressEvent.arrived!;
+        if (progressEvent.currentStepInstruction != null) {
+          _instruction = progressEvent.currentStepInstruction;
+        }
+        break;
+      case MapBoxEvent.route_building:
+      case MapBoxEvent.route_built:
+        _routeBuilt = true;
+        break;
+      case MapBoxEvent.route_build_failed:
+        _routeBuilt = false;
+        break;
+      case MapBoxEvent.navigation_running:
+        _isNavigating = true;
+        break;
+      case MapBoxEvent.on_arrival:
+        _arrived = true;
+        if (!_isMultipleStop) {
+          await Future.delayed(const Duration(seconds: 3));
+          await _controller?.finishNavigation();
+        } else {}
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        _routeBuilt = false;
+        _isNavigating = false;
+        break;
+      default:
+        break;
+    }
+    //refresh UI
+    setState(() {});
+  }
+}
